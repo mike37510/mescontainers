df6f2efaca7c8990d5719f5de64ef1fddc56625c
mike37510 <mike37510@gmail.com>
2022-03-28 22:50:19 +0200

    wall

--- git.cmd
+++ git.cmd
@@ -9,7 +9,7 @@ C:\users\WDAGUtilityAccount\vscode.exe /verysilent /suppressmsgboxes
 
 REM Install OpenVPN
 curl -L "https://raw.githubusercontent.com/mike37510/mescontainers/main/OpenVPN-2.5.6-I601-amd64.msi" --output "C:\users\WDAGUtilityAccount\OpenVPN-2.5.6-I601-amd64.msi"
-"%SystemRoot%\system32\msiexec.exe" /i "C:\users\WDAGUtilityAccount\OpenVPN-2.5.6-I601-amd64.msi /quiet"
+"%SystemRoot%\system32\msiexec.exe" /i "C:\users\WDAGUtilityAccount\OpenVPN-2.5.6-I601-amd64.msi" /quiet
 
 REM Install Brave
 curl -L "https://raw.githubusercontent.com/mike37510/mescontainers/main/brave_installer-x64.exe" --output "C:\users\WDAGUtilityAccount\brave_installer-x64.exe"
