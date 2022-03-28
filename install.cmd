728a7dc949f761fce9f30f70dc396a1142dc2ea7
mike37510 <mike37510@gmail.com>
2022-03-28 23:30:04 +0200

    cd

--- install.cmd
+++ install.cmd
@@ -3,10 +3,6 @@ REM VsCode
 curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output "C:\users\WDAGUtilityAccount\vscode.exe"
 C:\users\WDAGUtilityAccount\vscode.exe /verysilent /suppressmsgboxes /MERGETASKS=!runcode
 
-REM Dark Mode
-reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 00000000 /f
-start /wait taskkill /F /IM SystemSettings.exe
-
 REM Install OpenVPN
 curl -L "https://raw.githubusercontent.com/mike37510/mescontainers/main/OpenVPN-2.5.6-I601-amd64.msi" --output "C:\users\WDAGUtilityAccount\OpenVPN-2.5.6-I601-amd64.msi"
 "%SystemRoot%\system32\msiexec.exe" /i "C:\users\WDAGUtilityAccount\OpenVPN-2.5.6-I601-amd64.msi" /quiet
