a179ffe7bfd6b6be52570603bee1871c801030dd
mike37510 <mike37510@gmail.com>
2022-03-28 20:29:37 +0200

    test3

--- git.cmd
+++ git.cmd
@@ -4,5 +4,5 @@ curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --ou
 
 REM Install and run Visual Studio Code
 C:\users\WDAGUtilityAccount\vscode.exe /verysilent /suppressmsgboxes
-code --install-extension MS-CEINTL.vscode-language-pack-fr
-code --install-extension ms-python.python
+code --install-extension MS-CEINTL.vscode-language-pack-fr --force
+code --install-extension ms-python.python --force
