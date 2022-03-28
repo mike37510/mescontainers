58397a5f7cd209ca3da1c832e8a5300efea1ab2b
mike37510 <mike37510@gmail.com>
2022-03-28 23:07:45 +0200

    sd

--- git.cmd
+++ git.cmd
@@ -15,7 +15,3 @@ REM Install Brave
 curl -L "https://raw.githubusercontent.com/mike37510/mescontainers/main/brave_installer-x64.exe" --output "C:\users\WDAGUtilityAccount\brave_installer-x64.exe"
 "%SystemRoot%\system32\cmd.exe" /c "C:\users\WDAGUtilityAccount\brave_installer-x64.exe" --silent
 
-
-REM Wallpaper
-curl -L "https://raw.githubusercontent.com/mike37510/mescontainers/main/wallpaper.jpg" --output "C:\users\WDAGUtilityAccount\wallpaper.jpg"
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\users\WDAGUtilityAccount\wallpaper.jpg /f
