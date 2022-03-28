55cec72122290746d623f15ad706c53ee5b88457
mike37510 <mike37510@gmail.com>
2022-03-28 20:48:14 +0200

    test10

--- git.cmd
+++ git.cmd
@@ -3,4 +3,4 @@ REM Download Visual Studio Code
 curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output "C:\users\WDAGUtilityAccount\vscode.exe"
 
 REM Install and run Visual Studio Code
-C:\users\WDAGUtilityAccount\vscode.exe /verysilent /suppressmsgboxes /closeapplications
+C:\users\WDAGUtilityAccount\vscode.exe /verysilent /suppressmsgboxes /MERGETASKS=!runcode
